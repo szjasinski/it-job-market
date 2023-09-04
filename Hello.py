d501bf9f9ba0db5a66f4b0c585a4089b842fd1ce
Szymon Jasinski <szjasinski@outlook.com>
2023-09-04 17:07:34 +0200

    adds content to starting page

--- Hello.py
+++ Hello.py
@@ -8,42 +8,43 @@ from geopy.extra.rate_limiter import RateLimiter
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
+        'About': "# This is IT-job-market project!"
     }
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to my IT-job-market project! 👋")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select a page above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    The project is about getting insight into the state of polish IT job market.
+    **👈 Select a page from the sidebar** to find a job or see interesting plots and data! 
+    All offers are scraped from pracuj.pl website.
+    Feel free to see the code on my [github](https://github.com/szjasinski/it-job-market).
+    ### Want to find a new job?
+    - Every job offer has a salary brackets and link to page with offer details!
+    - You can sort offers by salary or filter them by salary, job title, employer and contract type!
+    - Download all data to csv file and explore it yourself!
+    ### Plots and statistics!
+    - See the median salary and salary distribution plot!
+    - Which employers have the most offers and which offer the highest salary on average?
+    - See location of companies' headquarters on the map!
 """
 )
 
-st.write("TO DO: oop app, geo data, data source column, sourcing data from nofluffjobs, rocketjobs, "
-                 "justjoinit, days to expiration, seaborn plots, schedule, PDF report generation, export report "
-                 "button, show plots and statistics buttton, plot of the most popular words in job offer"
-                 "export pdf report (images), deploy...")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write("TO DO (?): object oriented design, new data sources (nofluffjobs, rocketjobs, "
+         "justjoinit), schedule offers scraping, most popular words from job titles,"
+         "export plots as pdf report (images), make plot functions have arguments, monitor day to day changes in "
+         "offer num/quality, "
+         "map cirlce size slider...")
+st.caption('Created by Szymon Jasinski')
