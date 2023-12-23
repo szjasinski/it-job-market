d5355a5811167b56b852097a606cc58cb9eda0f5
Szymon Jasinski <szjasinski@outlook.com>
2023-12-23 20:22:35 +0100

    adds many new fields (eg requirements, benefits) to scrape

--- it_job_market/it_job_market/spiders/pracuj.py
+++ it_job_market/it_job_market/spiders/pracuj.py
@@ -22,6 +22,7 @@ class PracujSpider(CrawlSpider):
 
     def parse_item(self, response):
         item = ItJobMarketItem()
+
         item["job_title"] = response.xpath('//h1[@data-scroll-id="job-title"]/text()').get()
         item["employer"] = response.xpath('//h2[@data-scroll-id="employer-name"]/text()').get()
         item["min_salary"] = response.xpath('//span[@data-test="text-earningAmountValueFrom"]/text()').get()
@@ -30,9 +31,36 @@ class PracujSpider(CrawlSpider):
         item["contract_type"] = response.xpath('//div[@data-test="sections-benefit-contracts-text"]/text()').get()
         item["city"] = response.xpath('//div[@data-scroll-id="workplaces"]/div/div[@data-test="text-benefit"]/text()').get()
         item["expiration_date"] = response.xpath('//div[@data-scroll-id="publication-dates"]/div/div[2]/text()').get()
+        item["work_schedule"] = response.xpath('//div[@data-test="sections-benefit-work-schedule"]/div/div/text()').get()
+        item["position_level"] = response.xpath(
+            '//div[@data-test="sections-benefit-employment-type-name"]/div/div/text()').get()
+        item["work_mode"] = response.xpath(
+            '//div[@data-test="sections-benefit-work-modes"]/div/div/text()').get()
+        item["specialization"] = response.xpath(
+            '//li[@data-test="it-specializations"]/span[2]/text()').get()
+
+        item["expected_technologies"] = response.xpath(
+            '//div[@data-test="section-technologies-expected"]/ul/li/p/text()').getall()
+        item["optional_technologies"] = response.xpath(
+            '//div[@data-test="section-technologies-optional"]/ul/li/p/text()').getall()
+        item["about_project"] = response.xpath('//div[@data-test="text-about-project"]/p/text()').get()
+        item["responsibilities"] = response.xpath(
+            '//div[@data-test="section-responsibilities"]/ul/li/p/text()').getall()
+        item["expected_requirements"] = response.xpath(
+            '//div[@data-test="section-requirements-expected"]/ul/li/p/text()').getall()
+        item["optional_requirements"] = response.xpath(
+            '//div[@data-test="section-requirements-optional"]/ul/li/p/text()').getall()
+        item["development_opportunities"] = response.xpath(
+            '//div[@data-test="section-training-space"]/div/ul/li/p/text()').getall()
+        item["what_they_offer"] = response.xpath(
+            '//div[@data-test="section-offered"]/div/ul/li/p/text()').getall()
+        item["benefits"] = response.xpath(
+            '//section[@data-test="section-benefits"]/ul/li/article/p/text()').getall()
+
         item["url"] = response.url
         item["source"] = "pracuj.pl"
         item["scraping_datetime"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
 
         direct_address = response.xpath('//div[@data-scroll-id="workplaces"]/div/a/text()').get()
         if direct_address:
